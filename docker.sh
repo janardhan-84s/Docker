9168eacd18575aac5dcefa9fc38a1cef30c69362
Janardhan <janardhanj265@gmail.com>
2025-07-28 14:06:27 +0530

    docker

--- docker.sh
+++ docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Installing Docker in server
 dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
