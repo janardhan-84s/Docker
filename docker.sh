7d322b8ea49ea70b6ed1d6888d9816a961d16570
Janardhan <janardhanj265@gmail.com>
2025-08-06 12:35:09 +0530

    docker

--- docker.sh
+++ docker.sh
@@ -29,4 +29,8 @@ mv kubectl /usr/local/bin/kubectl
 eksctl version
 kubectl version
 
+# Installing Helm
 
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
